c528a8a3c80741906c821e64687ffb4e3a37c2b5
giampaolo <giampaolo@a13ad8ac-ce12-0410-a602-c8c22c8f6e20>
2010-07-21 13:44:59 +0000

    progress
    git-svn-id: http://dev.inghist.nl/svn/BiografischPortaal/buildout@17167 a13ad8ac-ce12-0410-a602-c8c22c8f6e20

--- srcman.py
+++ srcman.py
@@ -1,32 +1,136 @@
+#!/usr/bin/env python2.5
+
+"""Sources manager script"""
+
 import optparse
+import sys
+import os
 
+import biodes
 from bioport_repository.repository import Repository
+from bioport_repository.illustration import CantDownloadImage
+from bioport_repository.biography import Biography
 
 
-IMAGES_DIR = "/var/bioport/images_test"
-SQL_DB = "mysql://root@localhost/bioport"
+DEFAULT_IMAGES_DIR = "/var/bioport/images_test"
+DEFAULT_DB = "mysql://root@localhost/bioport"
+#TOTAL_BIOS = _repo.count_biographies()
+#TOTAL_PERSONS = _repo.count_persons()
+#IMAGES_DIR = _repo.images_cache_local  
 
-HELP_PROGRAM = """help program"""
 
+def get_ids():
+    """Return the IDs of all defined sources"""
+    return [src.id for src in _repo.get_sources()]
+       
 
-def list_sources(repo):
-    print
-    print "ID        QUALITY    DESCRIPTION"
-    print "================================="
-    print
-    for src in repo.get_sources(order_by="id", desc=True):
-        line = "%-9s %-10s %-5s\n" % (src.id, src.quality, src.description)
+def list_sources():
+    print "\ntotal biographies: %s"  %_repo.count_biographies()
+    print "\ntotal persons: %s\n" %_repo.count_persons()
+    print "ID        QUANTITY   QUALITY  DESCRIPTION"
+    print "=========================================\n"
+    for src in _repo.get_sources(order_by="id", desc=True):
+        quantity = _repo.count_biographies(source=src)        
+        line = "%-9s %-10s %-8s %-5s\n" % (src.id, quantity, src.quality, 
+                                           src.description)
         line = line.replace("\r\n", "")
         print line
 
-if __name__ == '__main__':
-    repo = Repository(db_connection=SQL_DB, images_cache_local=IMAGES_DIR)
-    usage = "python -m pyftpdlib.ftpserver [options]"
-    parser = optparse.OptionParser(usage=usage, description=HELP_PROGRAM)
+
+def download_illustrations(id, overwrite):
+    if id not in get_ids():
+        raise ValueError("No source with id '%s' was found" % id)
+    for src in _repo.get_sources():
+        if src.id == id:
+            break
+    bios = _repo.get_biographies(source=src)
+    total = len(bios)
+    skipped = 0
+    for index, bio in enumerate(bios):   
+        print "%s/%s " % (index + 1, total)
+        for ill in bio.get_illustrations():
+            try:
+                ill.download(overwrite=overwrite)
+            except CantDownloadImage, err:
+                skipped += 1
+                print err
+    print "total:%(total)s skipped:%(skipped)s" % locals()
+    
+    
+def download_biographies(id):
+    if id not in get_ids():
+        raise ValueError("No source with id '%s' was found" % id)
+    for src in _repo.get_sources():
+        if src.id == id:
+            break
+
+    ls = biodes.parse_list(src.url)
+    total = len(ls)
+    skipped = 0
+    for index, biourl in enumerate(ls):
+        print "%s/%s " % (index + 1, total)
+        if not biourl.startswith("http:"):
+            # we're dealing with a fs path
+            biourl = os.path.normpath(biourl)
+            if not os.path.isabs(biourl):
+                biourl = os.path.join(os.path.dirname(src.url), biourl)
+
+        bio = Biography(source_id=src.id, repository=src.repository)
+        try:
+            bio.from_url(biourl)
+        except Exception, err:
+            skipped += 1
+            print err
+            continue
+
+        _repo.add_biography(bio)
+
+    print "total:%(total)s skipped:%(skipped)s" % locals()
+    _repo.delete_orphaned_persons(source_id=src.id)
+
+    
+def main():
+    global _repo
+    class CustomizedOptionFormatter(optparse.IndentedHelpFormatter):
+        """Formats options shown in help in a prettier way."""
+
+        def format_option(self, option):
+            result = []
+            opts = self.option_strings[option]
+            result.append('  %s\n' % opts)
+            if option.help:
+                help_text = '     %s\n\n' %self.expand_default(option)
+                result.append(help_text)
+            return ''.join(result)
+    
+    parser = optparse.OptionParser()
     parser.add_option('-l', '--list', action='store_true',
                       help="list all available sources")
-
+    parser.add_option('-i', '--illustrations', metavar="ID",
+                      help="download illustrations of source ID")
+    parser.add_option('-b', '--biographies', metavar="ID",
+                      help="download biogrpahies of source ID")
+    parser.add_option('-o', '--overwrite', default=False, action='store_true',
+                      help="if specified re-download and overwrite existing "\
+                            "files when downloading illustrations" )
+    parser.add_option('-s', '--sqldb', default=DEFAULT_DB, metavar="DBURL",
+                      help='sql database connection url (defaults to %s)' % DEFAULT_DB)
+    parser.add_option('-d', '--directory', default=DEFAULT_IMAGES_DIR, metavar="DIRECTORY",
+                      help='the directory where the illustrations will be stored ' \
+                           '(defaults to %s)' % DEFAULT_IMAGES_DIR)
     options, args = parser.parse_args()
+    _repo = Repository(db_connection=options.sqldb, 
+                       images_cache_local=options.directory) 
     if options.list:
-        list_sources(repo)
-    
+        list_sources()
+        sys.exit(0)
+    if options.illustrations:
+        download_illustrations(options.illustrations, options.overwrite)
+        sys.exit(0)
+    if options.biographies:
+        download_biographies(options.biographies)
+
+        
+if __name__ == '__main__':
+    main()
+
