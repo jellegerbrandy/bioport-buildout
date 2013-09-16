cb9d7e203c4d319419868a84a61dfa07b218f192
Jelle Gerbrandy <github@gerbrandy.com>
2013-09-16 16:20:49 +0200

    jenkins also should update packages

--- jenkins.sh
+++ jenkins.sh
@@ -7,7 +7,11 @@ virtualenv env
 env/bin/pip install -U setuptools
 rm src/bioport_repository/bioport_repository/tests/data/bioport_mysqldump.sql -f
 env/bin/python bootstrap.py
+
 bin/buildout  -vv -c jenkins.cfg
+cd src/bioport_repository/ && git pull && cd ../..
+cd src/bioport/ && git pull && cd ../..
+cd src/biodes/ && git pull && cd ../..
 mysqladmin drop bioport_test -f
 mysqladmin create bioport_test
 bin/coveragetest bin/xmltest --xml --no-color
